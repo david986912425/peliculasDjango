f5cc8b1a2e5b71e30396b284dc4e373235b7a7e7
dm7659746 <dm7659746@gmail.com>
2022-08-09 22:38:05 -0500

    make it better

--- staticfiles/js/main.js
+++ staticfiles/js/main.js
@@ -1,9 +1,20 @@
+var menu = document.querySelector('.menu-icon');
+var navbar = document.querySelector('.navbar');
+menu.onclick = () => {
+  navbar.classList.toggle('open-menu');
+  menu.classList.toggle('move');
+};
+window.onscroll = () => {
+  navbar.classList.remove('open-menu');
+  menu.classList.remove('move');
+}
+
 var swiper = new Swiper(".popular-content", {
   slidesPerView: 1,
   spaceBetween: 10,
   centeredSliders: true,
   autoplay: {
-    delay:2500,
+    delay:7500,
     disableOnInteraction: false,
   },
   pagination: {
@@ -24,15 +35,15 @@ var swiper = new Swiper(".popular-content", {
       spaceBetween: 10,
     },
     510:{
-      slidesPerView: 3,
+      slidesPerView: 2,
       spaceBetween: 10,
     },
     758:{
-      slidesPerView: 4,
+      slidesPerView: 3,
       spaceBetween: 15,
     },
     900:{
-      slidesPerView: 5,
+      slidesPerView: 4,
       spaceBetween: 10,
     },
 
