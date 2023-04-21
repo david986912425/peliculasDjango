d8713466c3f88635fde751d5f2af89c2a01a253a
david986912425 <66333142+david986912425@users.noreply.github.com>
2023-04-21 11:45:28 -0500

    Update settings.py

--- Movie/settings.py
+++ Movie/settings.py
@@ -79,15 +79,15 @@ WSGI_APPLICATION = 'Movie.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-'''
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-'''
 
+'''
 DATABASES = {
     'default': {
         #'ENGINE': 'django.db.backends.sqlite3',
@@ -99,6 +99,7 @@ DATABASES = {
         'PORT': '5432',
     }
 }
+'''
 
 
 # Password validation
